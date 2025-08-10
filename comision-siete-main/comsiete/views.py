b54beddd7ce0558c8deaced03a4343b13a247dbb
Santiago Cabaña <santiagocabana65@gmail.com>
2025-08-10 18:38:14 -0300

    se eliminan carpetas duplicadas antes de la fusion

--- comision-siete-main/comsiete/views.py
+++
@@ -1,8 +0,0 @@
-# Solicitud HTTP
-# Solicitudes entrantes -> request
-# Devolver respuestas -> response
-from django.shortcuts import render
-
-# Devolver la página principal de mi sitio.
-def inicio(request):
-    return render(request, 'index.html')
